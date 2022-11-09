3f34182c0b108b18c7e9ffaac822a84b51a2aef8
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-09 18:16:08 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 git pull
 npm install
-./makedoc.sh
+
+wget -O docs-mbl.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-compiler/main/README.html
+wget -O docs-smpl.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-smpl/main/README.html
+wget -O docs-sim.html https://raw.githubusercontent.com/mathebuddy/mathebuddy-simulator/main/README.html
+
+#old: ./makedoc.sh
